4302f8b3ed58c05242fcc1c5a891473dc1047108
Daria Chuprina <chuprina.dariia@gmail.com>
2025-03-30 14:46:29 +0200

    schema + requirements

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS Goods (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    descriptionOfGoods TEXT,
+    declarationGoodsItemNumber TEXT,
+    MRN TEXT,
+    TransportDocument TEXT,
+    SupportingDocument_1 TEXT,
+    SupportingDocument_2 TEXT
+);
